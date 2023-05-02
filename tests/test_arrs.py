33ac304c05b1fb681f43e0915a6841cd00073a07
roman <romanchrnkv@gmail.com>
2023-05-02 23:26:40 +0300

    change test

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -14,3 +14,11 @@ def test_get():
 def test_slice():
     assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
     assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+    assert arrs.my_slice([1, 2], 1) == [2]
+    assert arrs.my_slice([1, 2], 2) == []
+    assert arrs.my_slice([1, 2, 3, 4], -1, 3) == []
+    assert arrs.my_slice([1, 2, 3, 4], 0, 3) == [1, 2, 3]
+    assert arrs.my_slice([1], 4, 3) == []
+    assert arrs.my_slice(["1", "4"], -15, -2) == []
+    assert arrs.my_slice(["1", [1, 2, 3], 3], 0, 4) == ['1', [1, 2, 3], 3]
+    assert  arrs.my_slice([], 1,) == []
