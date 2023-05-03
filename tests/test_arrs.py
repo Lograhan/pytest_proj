a86ea08b13805426b3ffaf82d3c429d883e3aa39
roman <romanchrnkv@gmail.com>
2023-05-03 20:35:41 +0300

    change test_arrs.py, fixture

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -1,3 +1,5 @@
+
+
 import pytest
 
 from utils import arrs, dicts
@@ -10,21 +12,28 @@ def test_get():
         arrs.get([], 0, "test")
 
 
-def test_slice():
-    assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
-    assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
-    assert arrs.my_slice([1, 2], 1) == [2]
-    assert arrs.my_slice([1, 2], 2) == []
-    assert arrs.my_slice([1, 2, 3, 4], -1, 3) == []
-    assert arrs.my_slice([1, 2, 3, 4], 0, 3) == [1, 2, 3]
+a = [1, 2, 3, 4]
+
+
+@pytest.fixture
+def arrs_fixture():
+    return [1, 2, 3, 4, 5, 6]
+
+def test_slice(arrs_fixture):
+    assert arrs.my_slice(arrs_fixture, 1, 3) == [2, 3]
+    assert arrs.my_slice(arrs_fixture, -1, 3) == []
+    assert arrs.my_slice(arrs_fixture, 0, 3) == [1, 2, 3]
     assert arrs.my_slice([1], 4, 3) == []
     assert arrs.my_slice(["1", "4"], -15, -2) == []
     assert arrs.my_slice(["1", [1, 2, 3], 3], 0, 4) == ['1', [1, 2, 3], 3]
     assert arrs.my_slice([], 1, ) == []
 
 
-def test_get_val():
-    assert dicts.get_val({"dog": "puppy", "cat": "kitty", "1": "23", "4": "56"}, "4", 'git') == "56"
-    assert dicts.get_val({"dog": "puppy", "cat": "kitty", "1": "23", "4": "56"}, "dog", 'git') == "puppy"
-    assert dicts.get_val({"dog": "puppy", "cat": "kitty", "1": "23", "4": "56"}, 4, 'git') == "git"
+@pytest.fixture
+def get_fixture():
+    return {"dog": "puppy", "cat": "kitty", "1": "23", "4": "56"}
+def test_get_val(get_fixture):
+    assert dicts.get_val(get_fixture, "4", 'git') == "56"
+    assert dicts.get_val(get_fixture, "dog", 'git') == "puppy"
+    assert dicts.get_val(get_fixture, 4, 'git') == "git"
     assert dicts.get_val({}, 4, 'git') == "git"
